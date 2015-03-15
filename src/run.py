af3dd33751663140d53e51ce840f5a552282aedb
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-03-15 18:36:57 +0000

    First processing of Reuters collection with nltk

--- src/run.py
+++ src/run.py
@@ -1,11 +1,21 @@
+import nltk.classify.util 
+from nltk.corpus import reuters 
+from nltk.classify import NaiveBayesClassifier 
+
 def main():
-	print("Script to run the experiments")	
+	print("Script to run the experiments");
 
-	# Load the data
+	print(reuters.categories());
+	print(reuters.categories());
 
-	# Run dKnn, kNN and SVM
+	train = set()
+	for category in reuters.categories():
+		print(category + " == " + str(len(reuters.fileids(category))))
+		for fileid in reuters.fileids(category):
+			if (fileid.startswith("train")):
+				train.add(fileid);
 
-	# Compare all results
+	print(len(train));					
 
 if __name__=='__main__':
     main()
